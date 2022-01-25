adac676fb9d0f09957cf5ee5e91bec0437b993b9
kanikachouhan <65353976+kanikachouhan@users.noreply.github.com>
2022-01-26 00:33:44 +0530

    Add files via upload

---
+++ speakgoodBye.js
@@ -0,0 +1,10 @@
+(function (window) {
+    var speakWord = "Good Bye";
+    var byeSpeaker = {
+      speak: function speak(name) {
+        console.log(speakWord + " " + name);
+      }
+    };
+    window.byeSpeaker = byeSpeaker;
+  })(window);
+  
